abf8238285bad72965f046266c0e8f248cfb1b95
Afo Oshun <afoyenyen@Afos-iMac.local>
2020-05-23 21:41:53 +0100

    on submit function added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Design from "./component/design";
+//import Design from "./component/design";
 import TaskForm from "./component/TaskForm";
 import TaskList from "./component/TaskList";
 import CompletedList from "./component/CompletedList";
@@ -10,24 +10,48 @@ import "./App.css";
 const initialTask = () => [
 	{
 		id: uuid(),
-		item: "go shooping",
+		chore: "go shooping",
 	},
 
 	{
 		id: uuid(),
-		item: "go to church",
+		chore: "go to church",
 	},
 ];
 
 function App() {
-	const [tasks, setTask] = useState(initialTask);
+	//set up states
+	const [tasks, setTasks] = useState(initialTask);
 
 	const [todoItem, setItem] = useState("");
 
+	const [alert, setAlert] = useState({ show: false });
+
+	const [chore, setChore] = useState("");
+
+	//set up functionality
+
+	const handleChore = (e) => {
+		setChore(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (chore !== "") {
+			const tempChore = { id: uuid(), chore };
+			setTasks([...tasks, tempChore]);
+			setChore("");
+		} else {
+		}
+	};
+
 	return (
 		<div>
-			<Design />
-			<TaskForm />
+			<TaskForm
+				chore={chore}
+				handleChore={handleChore}
+				handleSubmit={handleSubmit}
+			/>
 			<TaskList tasks={tasks} />
 			<CompletedList tasks={tasks} />{" "}
 		</div>
